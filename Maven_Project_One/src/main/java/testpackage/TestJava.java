0cafa41f0007cd58d955ecfdb795c8ef3cbf2552
HP <HP@192.168.1.36>
2024-01-17 08:31:38 +0530

    Test commit

--- Maven_Project_One/src/main/java/testpackage/TestJava.java
+++ Maven_Project_One/src/main/java/testpackage/TestJava.java
@@ -3,6 +3,7 @@ package testpackage;
 public class TestJava {
 
 	public static void main(String[] args) {
+		System.out.println("Start of statement");
 		System.out.println("End of statement");
 
 	}
